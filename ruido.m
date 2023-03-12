1a83d45e7045eab187a443f8f876e38069275edb
Ghilga <leosaugusto98@gmail.com>
2023-03-12 14:41:18 -0300

    add comentarios na função ruido

--- ruido.m
+++ ruido.m
@@ -1,9 +1,14 @@
-function saida = ruido(entrada, type, num_bits_codif, M,i)
+% NA: vetor de amplitudes do ruido
+% type: tipo de ruido
+% num_bits_codif: numero de bits do sinal codificado
+% M: numero de bits por símbolo
+% i: indice do vetor de amplitudes
+function saida = ruido(NA, type, num_bits_codif, M, i)
     if(type == 1)
-        saida = entrada(i) *complex(randn(1, num_bits_codif/M), randn(1, num_bits_codif/M))*sqrt(0.5);
+        saida = NA(i) *complex(randn(1, num_bits_codif/M), randn(1, num_bits_codif/M))*sqrt(0.5);
     end
     if(type == 2)  %Ruido Gaussiano Branco 
-        ruido = awgn(entrada, 2, -5); %sinal de entrada, relação sinal-ruído e potência do sinal de entrada
+        ruido = awgn(NA, 2, -5); %sinal de entrada, relação sinal-ruído e potência do sinal de entrada
         saida = ruido(i);
     end
 end
