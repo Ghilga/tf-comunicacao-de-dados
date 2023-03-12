dbcf5fc71a3cee9458b3a098d304fd00b3abeb04
Ghilga <leosaugusto98@gmail.com>
2023-03-12 15:46:33 -0300

    criar template do codigo final do trabalho

--- calcular_ber.m
+++ calcular_ber.m
@@ -6,7 +6,8 @@
 % M: quantidade de bits por simbolo
 % Eb: energia por bit para a modulacao BPSK utilizada
 % ber: bit error rate calculado para cada iteracao
-function ber = calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_codif, M, Eb, Eb_N0_dB)
+% tipo_mod: tipo de modulacao (QAM ou PSK)
+function ber = calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_codif, M, Eb, Eb_N0_dB, tipo_mod)
     Eb_N0_lin = 10 .^ (Eb_N0_dB/10); %faixa de Eb/N0 linearizada
     ber = zeros(size(Eb_N0_lin)); %pre-alocacao do vetor de BER
     
@@ -16,7 +17,11 @@ function ber = calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_
     for i = 1:length(Eb_N0_lin)
         n = ruido(NA, 1, num_bits_codif, M, i); %vetor de ru�do complexo com desvio padr�o igual a uma posi��o do vetor NA
         r = entrada_modulada + n; % vetor recebido
-        demod = demodulador_qam(r, M); % recupera a informacao (sinal demodulado)
+        if strcmp(tipo_mod, 'qam')
+            demod = demodulador_qam(r, M); % recupera a informacao (sinal demodulado)
+        else
+            demod = demodulador_psk(r, M);
+        end
         saida_demodulada = reshape(de2bi(demod, 'left-msb'), 1, num_bits_codif); % converte a saida demodulada para bits
         saida_decodificada = decodificador(saida_demodulada, trellis, num_b); % decodifica a entrada demodulada
         ber(i) = sum(entrada ~= saida_decodificada) / num_b; % contagem de erros e calculo do BER
--- main.m
+++ main.m
@@ -2,42 +2,72 @@ clear;
 close all;
 
 num_b = 1000000; %número de bits a serem simulados
-M = 4; % quantidade de bits por simbolo (utilizado para definir a dimensão da modulação)
 entrada = fonte(num_b);
+M = 4; % quantidade de bits por simbolo (utilizado para definir a dimensão da modulação)
 Eb = 10/4; % energia por bit para o 16-QAM
 Eb_N0_dB = -4:1:12; %faixa de Eb/N0
 Eb_N0_lin = 10 .^ (Eb_N0_dB/10); %faixa de Eb/N0 linearizada
 
-% 16-QAM com codigo (2,2,3)
-trellis = criar_trellis(2,2,3);
-entrada_codificada = codificador(entrada, trellis); % codificador convolucional
-entrada_modulada = modulador_qam(entrada_codificada, M);
-num_bits_codif = length(entrada_codificada); % numero de bits no sinal codificado
-ber_qam_cod1 = calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_codif, M, Eb, Eb_N0_dB);
+% Inicializar a treliça para cada codificação.
+% Aqui sao definidos os 3 codigos convolucionais a serem usados na
+% simulação
+trellis(1) = criar_trellis(2,2,3);
+trellis(2) = criar_trellis(2,2,3);
+trellis(3) = criar_trellis(2,2,3);
 
-% 16-QAM com codigo ---
-% trellis = criar_trellis(---);
-% entrada_codificada = codificador(entrada, trellis); % codificador convolucional
-% entrada_modulada = modulador_qam(entrada_codificada, M);
-% num_bits_codif = length(entrada_codificada); % numero de bits no sinal codificado
-% ber_qam_cod1 = calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_codif, M, Eb, Eb_N0_dB);
+% Criar as entradas codificadas para cada codigo convolucional
+entrada_codificada = cell(1,3);
+for i = 1:3
+    entrada_codificada{i} = codificador(entrada, trellis(i));
+end
 
-% 16-QAM com codigo ---
-% trellis = criar_trellis(---);
-% entrada_codificada = codificador(entrada, trellis); % codificador convolucional
-% entrada_modulada = modulador_qam(entrada_codificada, M);
-% num_bits_codif = length(entrada_codificada); % numero de bits no sinal codificado
-% ber_qam_cod1 = calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_codif, M, Eb, Eb_N0_dB);
+% Calcular os BER para cada codigo usando QAM
+ber_qam_cod = cell(1,3);
+for i = 1:3
+    entrada_modulada = modulador_qam(entrada_codificada{i}, M);
+    num_bits_codif = length(entrada_codificada{i}); % numero de bits no sinal codificado
+    ber_qam_cod{i} = calcular_ber(entrada, entrada_modulada, trellis(i), num_b, num_bits_codif, M, Eb, Eb_N0_dB, 'qam');
+end
 
 % 16-QAM sem codigo
 entrada_modulada = modulador_qam(entrada, M);
-ber_qam_sem_cod = calcular_ber_sem_cod(entrada, entrada_modulada, num_b, M, Eb, Eb_N0_dB);
+ber_qam_sem_cod = calcular_ber_sem_cod(entrada, entrada_modulada, num_b, M, Eb, Eb_N0_dB, 'qam');
 
+% -------------------------------//----------------------------//-------------------------
+
+% Calcular os BER para cada codigo usando PSK
+ber_psk_cod = cell(1,3);
+% for i = 1:3
+%     entrada_modulada = modulador_psk(entrada_codificada{i}, M);
+%     num_bits_codif = length(entrada_codificada{i}); % numero de bits no sinal codificado
+%     ber_psk_cod{i} = calcular_ber(entrada, entrada_modulada, trellis(i), num_b, num_bits_codif, M, Eb, Eb_N0_dB, 'psk');
+% end
+
+% M-PSK sem codigo
+% entrada_modulada = modulador_psk(entrada, M);
+% ber_psk_sem_cod = calcular_ber_sem_cod(entrada, entrada_modulada, num_b, M, Eb, Eb_N0_dB, 'psk');
+
+% Colocar todos os BER da modulacao QAM em uma matriz para plotar mais facilmente
+ber_qam = vertcat(ber_qam_cod{1}, ber_qam_cod{2}, ber_qam_cod{3}, ber_qam_sem_cod); 
+% Colocar todos os BER da modulacao PSK em uma matriz para plotar mais facilmente
+% ber_psk = vertcat(ber_psk_cod{1}, ber_psk_cod{2}, ber_psk_cod{3}, ber_psk_sem_cod); 
+
+ber_theoretical = 0.5*erfc(sqrt(2*Eb_N0_lin)/sqrt(2)); %BER teorico para comparacao
 
 % Plot dos graficos QAM
-ber_theoretical = 0.5*erfc(sqrt(2*Eb_N0_lin)/sqrt(2)); %BER teorico para comparacao
 figure;
-semilogy(Eb_N0_dB, ber_qam_cod1, 'x', Eb_N0_dB, ber_qam_sem_cod, 'x', Eb_N0_dB, ber_theoretical, 'r', 'LineWidth', 2, 'MarkerSize', 10);
+% semilogy(Eb_N0_dB, ber_qam_cod{1}, 'x', Eb_N0_dB, ber_qam_cod{2}, 'x', Eb_N0_dB, ber_qam_cod{3}, 'x', Eb_N0_dB, ber_qam_sem_cod, 'x', Eb_N0_dB, ber_theoretical, 'r', 'LineWidth', 2, 'MarkerSize', 10);
+semilogy(Eb_N0_dB, ber_qam, 'x', Eb_N0_dB, ber_theoretical, 'r', 'LineWidth', 2, 'MarkerSize', 10);
 xlabel('Eb/N0 (dB)');
 ylabel('BER');
-legend('QAM(2,2,3)','QAM(sem cod)','Teorico');
+legend('QAM(2,2,3)','QAM(-,-,-)','QAM(-,-,-)','QAM(sem cod)','Teorico');
+
+% Plot dos graficos PSK
+% figure;
+% semilogy(Eb_N0_dB, ber_psk, 'x', Eb_N0_dB, ber_theoretical, 'r', 'LineWidth', 2, 'MarkerSize', 10);
+% xlabel('Eb/N0 (dB)');
+% ylabel('BER');
+% legend('PSK(2,2,3)','PSK(-,-,-)','PSK(-,-,-)','PSK(sem cod)','Teorico');
+
+
+
