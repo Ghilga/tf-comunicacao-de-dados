63099093dfc096cd2d31321a483a525a0f44772d
Ghilga <leosaugusto98@gmail.com>
2023-03-12 14:41:07 -0300

    add templates para modulacoes qam

--- calcular_ber_sem_cod.m
+++ calcular_ber_sem_cod.m
@@ -6,7 +6,7 @@
 % M: quantidade de bits por simbolo
 % Eb: energia por bit para a modulacao BPSK utilizada
 % ber: bit error rate calculado para cada iteracao
-function ber = calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_codif, M, Eb, Eb_N0_dB)
+function ber = calcular_ber_sem_cod(entrada, entrada_modulada, num_b, M, Eb, Eb_N0_dB)
     Eb_N0_lin = 10 .^ (Eb_N0_dB/10); %faixa de Eb/N0 linearizada
     ber = zeros(size(Eb_N0_lin)); %pre-alocacao do vetor de BER
     
@@ -14,11 +14,10 @@ function ber = calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_
     NA = sqrt(NP); %vetor de amplitudes do ruido
     
     for i = 1:length(Eb_N0_lin)
-        n = ruido(NA, 1, num_bits_codif, M, i); %vetor de ru�do complexo com desvio padr�o igual a uma posi��o do vetor NA
+        n = ruido(NA, 1, num_b, M, i); %vetor de ru�do complexo com desvio padr�o igual a uma posi��o do vetor NA
         r = entrada_modulada + n; % vetor recebido
         demod = demodulador_qam(r, M); % recupera a informacao (sinal demodulado)
-        saida_demodulada = reshape(de2bi(demod, 'left-msb'), 1, num_bits_codif); % converte a saida demodulada para bits
-        saida_decodificada = decodificador(saida_demodulada, trellis, num_b); % decodifica a entrada demodulada
-        ber(i) = sum(entrada ~= saida_decodificada) / num_b; % contagem de erros e calculo do BER
+        saida_demodulada = reshape(de2bi(demod, 'left-msb'), 1, num_b); % converte a saida demodulada para bits
+        ber(i) = sum(entrada ~= saida_demodulada) / num_b; % contagem de erros e calculo do BER
     end
 end
--- main.m
+++ main.m
@@ -4,9 +4,40 @@ close all;
 num_b = 1000000; %número de bits a serem simulados
 M = 4; % quantidade de bits por simbolo (utilizado para definir a dimensão da modulação)
 entrada = fonte(num_b);
+Eb = 10/4; % energia por bit para o 16-QAM
+Eb_N0_dB = -4:1:12; %faixa de Eb/N0
+Eb_N0_lin = 10 .^ (Eb_N0_dB/10); %faixa de Eb/N0 linearizada
+
+% 16-QAM com codigo (2,2,3)
 trellis = criar_trellis(2,2,3);
-entrada_codificada = codificador(entrada, trellis); % codificador convolucional (2,2,3)
-entrada_modulada = modulador_qam(entrada_codificada, M); %16-QAM basico, sem codificação
-num_bits_codif = length(entrada_codificada);
-Eb = 10/4;
-calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_codif, M, Eb);
+entrada_codificada = codificador(entrada, trellis); % codificador convolucional
+entrada_modulada = modulador_qam(entrada_codificada, M);
+num_bits_codif = length(entrada_codificada); % numero de bits no sinal codificado
+ber_qam_cod1 = calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_codif, M, Eb, Eb_N0_dB);
+
+% 16-QAM com codigo ---
+% trellis = criar_trellis(---);
+% entrada_codificada = codificador(entrada, trellis); % codificador convolucional
+% entrada_modulada = modulador_qam(entrada_codificada, M);
+% num_bits_codif = length(entrada_codificada); % numero de bits no sinal codificado
+% ber_qam_cod1 = calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_codif, M, Eb, Eb_N0_dB);
+
+% 16-QAM com codigo ---
+% trellis = criar_trellis(---);
+% entrada_codificada = codificador(entrada, trellis); % codificador convolucional
+% entrada_modulada = modulador_qam(entrada_codificada, M);
+% num_bits_codif = length(entrada_codificada); % numero de bits no sinal codificado
+% ber_qam_cod1 = calcular_ber(entrada, entrada_modulada, trellis, num_b, num_bits_codif, M, Eb, Eb_N0_dB);
+
+% 16-QAM sem codigo
+entrada_modulada = modulador_qam(entrada, M);
+ber_qam_sem_cod = calcular_ber_sem_cod(entrada, entrada_modulada, num_b, M, Eb, Eb_N0_dB);
+
+
+% Plot dos graficos QAM
+ber_theoretical = 0.5*erfc(sqrt(2*Eb_N0_lin)/sqrt(2)); %BER teorico para comparacao
+figure;
+semilogy(Eb_N0_dB, ber_qam_cod1, 'x', Eb_N0_dB, ber_qam_sem_cod, 'x', Eb_N0_dB, ber_theoretical, 'r', 'LineWidth', 2, 'MarkerSize', 10);
+xlabel('Eb/N0 (dB)');
+ylabel('BER');
+legend('QAM(2,2,3)','QAM(sem cod)','Teorico');
